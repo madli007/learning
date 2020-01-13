eff4767d97bc146d9035d318b2e6edd856bfd765
Matej Zgubič <matej.zgubic@alcad.si>
2020-01-13 14:20:16 +0100

    Initial project commit

---
+++ LearningGit/LearningGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

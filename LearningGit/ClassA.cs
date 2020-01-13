ab71ec848cef43421abe5343bae35df0e6f8df17
Matej Zgubič <matej.zgubic@alcad.si>
2020-01-13 14:28:48 +0100

    Added Class A

---
+++ LearningGit/ClassA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningGit
+{
+    class ClassA
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+
+        public ClassA(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("{0} {1}", Id, Name);
+        }
+    }
+}
